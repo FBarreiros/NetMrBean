5736c918e4f45385ce5bfa75af8178b58e4f6091
Admin <Admin@10.23.105.138>
2018-11-14 20:48:35 +0000

    Primeira versão do Mr. Bean

---
+++ netmrbean/NetMrBean.java
@@ -0,0 +1,11 @@
+package netmrbean;
+
+public class NetMrBean {
+static int i = 1;
+static int o = 2;
+    
+    public static void main(String[] args) {
+        System.out.println(o-i);
+    }
+    
+}
