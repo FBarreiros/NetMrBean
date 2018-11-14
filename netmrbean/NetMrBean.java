8db87b6c68edbb1dc573b10c3b5866f8bdbfdb66
Admin <Admin@10.23.105.138>
2018-11-14 22:05:02 +0000

    Segunda versão do Mr. Bean em que ele estraga tudo

--- netmrbean/NetMrBean.java
+++ netmrbean/NetMrBean.java
@@ -2,7 +2,7 @@ package netmrbean;
 
 public class NetMrBean {
 static int i = 1;
-static int o = 2;
+static int o = 2.5;
     
     public static void main(String[] args) {
         System.out.println(o-i);
