1c01834a220a5441f118d31028ec1cd79f89d3f5
Admin <Admin@10.23.105.138>
2018-11-14 22:08:33 +0000

    Terceira versão do Mr. Bean em que as coisas até ficam bem

--- netmrbean/NetMrBean.java
+++ netmrbean/NetMrBean.java
@@ -1,8 +1,8 @@
 package netmrbean;
 
 public class NetMrBean {
-static int i = 1;
-static int o = 2.5;
+static int i = 2;
+static int o = 4;
     
     public static void main(String[] args) {
         System.out.println(o-i);
